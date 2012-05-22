694d9866388d3c99ea5bdf2d570865f0bfa79d44
Bernd Zeimetz <bernd@bzed.de>
2012-05-22 18:22:12 +0200

    Refreshing patches and picking more bugfixes from git HEAD.
    Including a fix for a segfaults happening in the aivdm driver after
    compiling it with stack-protector and gcc 4.7.

--- debian/patches/1689ae9_Fix-type-error-in-ntpshm.c
+++ debian/patches/1689ae9_Fix-type-error-in-ntpshm.c
@@ -8,11 +8,9 @@ Subject: [PATCH] Fix type error in ntpshm.c Found by: Beat Bolli
  ntpshm.c |    2 +-
  1 file changed, 1 insertion(+), 1 deletion(-)
 
-diff --git a/ntpshm.c b/ntpshm.c
-index 6a6f741..d52c18f 100644
 --- a/ntpshm.c
 +++ b/ntpshm.c
-@@ -451,7 +451,7 @@ static int init_kernel_pps(struct gps_device_t *session) {
+@@ -451,7 +451,7 @@ static int init_kernel_pps(struct gps_de
      int ldisc = 18;   /* the PPS line discipline */
      pps_params_t pp;
      glob_t globbuf;
@@ -21,6 +19,3 @@ index 6a6f741..d52c18f 100644
      char pps_num = 0;     /* /dev/pps[pps_num] is our device */
      char path[GPS_PATH_MAX] = "";
  
--- 
-1.7.10
-
